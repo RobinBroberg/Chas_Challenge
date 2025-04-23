d7dc23683bec1827c3ba1b96fa669919d4e40f76
mmmmm <mustaf.abdulahi@chasacademy.se>
2025-04-23 15:27:00 +0200

    he11

---
+++ app/hr-login/page.js
@@ -0,0 +1,6 @@
+"use client";
+import CreateLoginForHR from "../CreateLoginForHR";
+
+export default function HRLoginPage() {
+  return <CreateLoginForHR />;
+}
---
+++ app/worker-login/page.js
@@ -0,0 +1,6 @@
+"use client";
+import CreateLoginForWorkers from "../CreateLoginForWorkers";
+
+export default function WorkerLoginPage() {
+  return <CreateLoginForWorkers />;
+}
