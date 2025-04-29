31d85664764c12cb8830483572450af83950e9b3
RobinBroberg <robin.broberg86@gmail.com>
2025-04-29 12:53:05 +0200

    fixed backend issue and added script for starting services

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import pool from "./config/db";
+import mysql from "mysql2/promise";
 import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// API Routes
+const { DB_USER, DB_PASS, DB_PORT, DB_HOST, DB_NAME } = process.env;
 
-// Hämtar användare från databasen
-app.get("/users", async (req, res) => {
-  try {
-    const [users] = await pool.execute(
-      "SELECT id, first_name, last_name, email, role FROM users"
-    );
-    res.json(users); // Returnera resultatet som JSON
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ message: "Failed to get users" });
-  }
+const pool = mysql.createPool({
+  user: DB_USER,
+  password: DB_PASS,
+  host: DB_HOST,
+  database: DB_NAME,
+  port: DB_PORT,
 });
 
-// Hämtar frågor från databasen
+async function query(sql, params) {
+  const [results] = await pool.execute(sql, params);
+  return results;
+}
+
+app.get("/users", async (req, res) => {
+  const users = await query(
+    "SELECT id, first_name, last_name, email, role FROM users"
+  );
+  res.json(users);
+});
+
 app.get("/questions", async (req, res) => {
   try {
-    const [questions] = await pool.execute("SELECT * FROM questions");
+    const questions = await query("SELECT * FROM questions");
     res.json(questions);
   } catch (error) {
     console.error("Error fetching questions:", error);
@@ -37,7 +42,6 @@ app.get("/questions", async (req, res) => {
   }
 });
 
-// Starta servern
 app.listen(port, () => {
   console.log(`CC backend running at http://localhost:${port}`);
 });
