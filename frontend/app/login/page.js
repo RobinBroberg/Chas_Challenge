3549c163674585a3dd413fc5fc620a2e7b6b8e95
Helin <helin.saedi@gmail.com>
2025-05-12 22:21:30 +0200

    Updated css login

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -2,6 +2,15 @@
 import React, { useState, useEffect } from "react";
 import { login, getCurrentUser } from "@/services/api";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'], 
+});
+
+
 
 const loginPage = () => {
   const [email, setEmail] = useState("");
@@ -40,11 +49,11 @@ const loginPage = () => {
   return (
     <div
       className="w-full h-screen flex items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/loginpic.png')" }}
+      style={{ backgroundImage: "url('/landingHeader.png')" }}
     >
-      <div className="flex flex-col space-y-6 mb-30">
+      <div className="flex flex-col space-y-6 mt-15">
         <h2
-          className="text-4xl font-medium text-left mb-18"
+          className="text-4xl font-medium text-left mb-18 font-montserrat"
           style={{ color: "#F6F4F0" }}
         >
           Logga in
@@ -52,10 +61,10 @@ const loginPage = () => {
 
         <input
           type="text"
-          placeholder="Email"
+          placeholder="Användarnamn"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="border-b-2 w-160 border-white p-2 text-lg focus:outline-none text-white placeholder-white focus:border-gray-300"
+          className="border-b-2 w-160 border-white p-2 text-lg focus:outline-none text-white placeholder-white focus:border-gray-300 font-montserrat"
         />
 
         <input
@@ -63,10 +72,17 @@ const loginPage = () => {
           placeholder="Lösenord"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="border-b-2 w-160 border-white p-2 text-lg focus:outline-none text-white placeholder-white focus:border-gray-300"
+          className="border-b-2 w-160 border-white p-2 text-lg focus:outline-none text-white placeholder-white focus:border-gray-300 font-montserrat"
         />
 
-        <div className="flex space-x-4 justify-center">
+        <div className="flex justify-end">
+          <Link href="/resetPassword" className="text-sm text-white hover:underline font-montserrat">
+            Glömt lösenord?
+            </Link>
+          </div>
+
+
+        <div className="flex space-x-6 justify-center">
           <button
             onClick={handleLogin}
             className="px-27 py-4 bg-gray-300 text-white rounded-full hover:bg-gray-400 transition font-bold text-lg"
@@ -74,14 +90,18 @@ const loginPage = () => {
           >
             Logga in
           </button>
+          
+          <Link href="/createAccount">
+
 
           <button
-            onClick={handleCreateAccount}
-            className="px-27 py-4 bg-gray-300 text-white rounded-full hover:bg-gray-400 transition font-bold text-lg"
-            style={{ color: "#47423E" }}
-          >
-            Skapa konto
-          </button>
+            className="px-27 py-4 bg-gray-300 text-white rounded-full hover:bg-gray-400 transition font-bold text-lg font-montserrat"
+           style={{ color: "#47423E" }}
+           >
+           Skapa konto
+           </button>
+            </Link>
+
         </div>
       </div>
     </div>
