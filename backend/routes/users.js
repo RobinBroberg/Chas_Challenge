0865c963e84e96687229cc609fda6310a1eb6697
RobinBroberg <robin.broberg86@gmail.com>
2025-05-13 10:10:23 +0200

    removed unused function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -22,43 +22,4 @@ router.get("/", requireAuth, requireRole("admin"), async (req, res) => {
   }
 });
 
-// POST /users - Endast admin kan skapa nya användare
-router.post("/", requireAuth, requireRole("admin"), async (req, res) => {
-  const { name, password, email } = req.body;
-  const { company_id } = req.user;
-
-  // Validera att namn, lösenord och email finns
-  if (!name || !password || !email) {
-    return res.status(400).json({ message: "Namn, email och lösenord krävs." });
-  }
-
-  // Kontrollera om användaren redan finns
-  try {
-    const existingUser = await query(
-      `SELECT * FROM users WHERE email = ? AND company_id = ?`,
-      [email, company_id]
-    );
-
-    if (existingUser.length > 0) {
-      return res
-        .status(400)
-        .json({ message: "Användare med denna e-post finns redan." });
-    }
-
-    // Hasher lösenordet
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Spara användaren i databasen
-    const result = await query(
-      `INSERT INTO users (first_name, email, password, company_id, role) VALUES (?, ?, ?, ?, ?)`,
-      [name, email, hashedPassword, company_id, "user"]
-    );
-
-    res.status(201).json({ id: result.insertId, name, email });
-  } catch (error) {
-    console.error("Fel vid skapande av användare:", error);
-    res.status(500).json({ message: "Kunde inte skapa användare." });
-  }
-});
-
 export default router;
