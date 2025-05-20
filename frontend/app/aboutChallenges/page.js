2a759b095c1e3a637fb63de5fe0436d7f118dd99
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-05-20 18:33:02 +0200

    extrapages, survey namechange

---
+++ frontend/app/aboutChallenges/page.js
@@ -0,0 +1,26 @@
+"use client";
+import ExtraSidePages from "@/components/ExtraSidePages";
+
+export default function AboutChallenges() {
+  return (
+    <ExtraSidePages
+      imageSrc="/utmaningImage.png"
+      title="UTMANINGAR"
+      text="Utmaningar på Balance är ett engagerande sätt att främja välmående och
+        gemenskap på arbetsplatsen. Genom att delta i olika typer av utmaningar
+        får medarbetare möjlighet att sätta mål för sin hälsa och träning,
+        samtidigt som de motiverar varandra och skapar en positiv arbetsmiljö.                        
+        På Balance kan medarbetare enkelt utmana varandra i exempelvis
+        steg-utmaningar. Genom att delta i dessa utmaningar får användare ett
+        roligt och motiverande sätt att förbättra sin hälsa och välmående,
+        samtidigt som de stärker teamkänslan på arbetsplatsen. Utmaningarna kan
+        vara individuella eller team-baserade, och de kan anpassas för att passa
+        olika behov och nivåer. Plattformen gör det enkelt att följa upp sina
+        framsteg, tävla mot kollegor eller helt enkelt delta för att förbättra
+        sitt eget välmående. Oavsett om målet är att ta fler steg varje dag
+        eller att hitta en bättre balans mellan arbete och hälsa,
+        ger Balance verktygen för att göra det på ett inspirerande och socialt
+        sätt."
+    ></ExtraSidePages>
+  );
+}
