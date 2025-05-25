3439870abc499c5b0feef83399b8690555247413
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-05-25 21:22:30 +0200

    contact + policy pages

---
+++ frontend/app/integritetspolicy/page.js
@@ -0,0 +1,55 @@
+export default function Integritetspolicy() {
+  return (
+    <div className="text-black bg-white p-20 ">
+      <h1 className="font-wix madefor display font-semibold text-3xl">
+        Integritetspolicy för Balance
+      </h1>
+      <h2 className="font-wix madefor display font-semibold text-2xl pt-12">
+        Vi bryr oss om din integritet!
+      </h2>
+      <div className="font-montserrat tracking-tight">
+        <p className="font-normal text-lg pt-10">
+          Välkommen till Balance! Din integritet är viktig för oss och vi vill
+          att du ska känna dig trygg med hur vi hanterar dina uppgifter.
+        </p>
+        <h3 className="font-semibold text-lg pt-5">
+          Vilken data samlar vi in?
+        </h3>
+        <p className="font-normal text-lg pt-5">
+          Vi samlar in personuppgifter (som namn och e-post) från din
+          arbetsgivare när de ansluter dig till Balance. Du loggar in som
+          anställd eller som HR/chef. Vi samlar också in anonyma svar från
+          medarbetarundersökningar - dessa kan aldrig kopplas till dig som
+          individ.
+        </p>
+        <h3 className="font-semibold text-lg pt-5">
+          Vad använder vi datan till?
+        </h3>
+        <p className="font-normal text-lg pt-5">
+          Vi använder datan för att:
+          <span className="flex flex-col pl-4">
+            <span>• Leverera och förbättra vår tjänst</span>
+            <span>
+              • Säkerställa att rätt person får tillgång till rätt funktioner
+            </span>
+          </span>
+        </p>
+        <p className="font-normal text-lg pt-5">
+          Vi delar inte din data med någon utanför Balance, som t.ex en tredje
+          part.
+        </p>
+
+        <h3 className="font-semibold text-lg pt-5">Vem ansvarar för datan?</h3>
+        <p className="font-normal text-lg pt-5">
+          Yrkeshögskolan bakom Balance ansvarar för hanteringen av
+          personuppgifter i enlighet med dataskyddslagar (GDPR).
+        </p>
+        <h3 className="font-semibold text-lg pt-5">Har du frågor?</h3>
+        <p className="font-normal text-lg pt-5">
+          Kontakta gärna oss eller din arbetsgivare om du har frågor om hur din
+          data används.
+        </p>
+      </div>
+    </div>
+  );
+}
