5039aed1eec82eeea9f7bb7fd558cf2b3769e446
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-05-26 13:16:08 +0200

    text

--- frontend/components/ImageTextSection.jsx
+++ frontend/components/ImageTextSection.jsx
@@ -5,8 +5,6 @@ import React from "react";
 const ImageTextSection = ({ imageSrc, title, undertitle, text }) => {
   return (
     <div className="flex w-full h-screen">
-
-     
       <div className="w-[45%] h-full">
         <img
           src={imageSrc}
@@ -19,13 +17,13 @@ const ImageTextSection = ({ imageSrc, title, undertitle, text }) => {
         <div className="max-w-2xl px-16">
           <h1 className="text-4xl font-bold text-black mb-25">{title}</h1>
           <h3 className="text-2xl font-bold text-black mb-15">{undertitle}</h3>
-          <p className="text-lg text-gray-700 font-Montserrat leading-relaxed mb-20">{text}</p>
+          <p className="text-lg text-gray-700 font-Montserrat leading-relaxed mb-20">
+            {text}
+          </p>
         </div>
       </div>
     </div>
   );
 };
 
-
 export default ImageTextSection;
-
