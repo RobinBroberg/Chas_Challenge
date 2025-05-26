004de7fb8901f3a87837fce0267b4a03fcb591a1
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-05-26 15:12:38 +0200

    all articles added

--- frontend/app/articles/page.js
+++ frontend/app/articles/page.js
@@ -3,13 +3,25 @@
 import React from "react";
 import ImageTextSection from "@/components/ImageTextSection";
 
-export default function microRests() {
+export default function MicroRests() {
   return (
-    <ImageTextSection
-      imageSrc="/mikropauser.png"
-      title=""
-      undertitle="MIKROPAUSER I VARDAGEN "
-      text="Mikropauser är medvetna pauser under dagen som ofta bara varar ett par minuter. Trots sin enkelhet har de stor effekt. En mikropaus kan bestå av att sträcka på kroppen, släppa blicken från skärmen, ta några djupa andetag och vara närvarande. I en arbetsvardag där vi ofta sitter länge, byter fokus snabbt och har höga krav, är mikropauser ett sätt att återhämta sig i stunden. De hjälper dig att minska spänningar i kroppen, reducera mental trötthet och bibehålla fokus längre. Att ta en mikropaus då och då är ett enkelt sätt att ta hand om sig själv utan att det stör arbetsflödet. Tvärtom kan det göra dig mer effektiv, mer fokuserad och balanserad under arbetsdagen."
-    />
+    <div>
+      <ImageTextSection
+        imageSrc="/mikropauser.png"
+        undertitle="MIKROPAUSER I VARDAGEN "
+        text="Mikropauser är medvetna pauser under dagen som ofta bara varar ett par
+        minuter. Trots sin enkelhet har de stor effekt. En mikropaus kan bestå
+        av att sträcka på kroppen, släppa blicken från skärmen, ta några djupa
+        andetag och vara närvarande."
+        text2="I en arbetsvardag där vi ofta sitter länge,
+        byter fokus snabbt och har höga krav, är mikropauser ett sätt att
+        återhämta sig i stunden. De hjälper dig att minska spänningar i kroppen,
+        reducera mental trötthet och bibehålla fokus längre."
+        text3="Att ta en mikropaus
+        då och då är ett enkelt sätt att ta hand om sig själv utan att det stör
+        arbetsflödet. Tvärtom kan det göra dig mer effektiv, mer fokuserad och
+        balanserad under arbetsdagen."
+      />
+    </div>
   );
 }
---
+++ frontend/app/articles/sleep/page.js
@@ -0,0 +1,73 @@
+"use client";
+
+export default function Sleep() {
+  return (
+    <div className="bg-white text-black">
+      <img
+        src="/sleepImage.png"
+        className="w-full h-[350px] object-cover"
+      ></img>
+      <div className="flex flex-col px-24 py-20 tracking-wider">
+        <h1 className="font-extrabold text-2xl font-wix madefor display">
+          SÖMN ÄR GRUNDEN FÖR ÅTERHÄMTNING
+        </h1>
+        <h2 className="font-bold text-lg pt-8 font-montserrat">
+          Varför behöver vi sömn?
+        </h2>
+        <p className="font-medium text-lg w-5xl pt-6 font-montserrat">
+          Sömn påverkar hur vi mår, tänker och presterar. När vi sover bra har
+          vi lättare att fokusera, fatta beslut och hantera stress. Dålig sömn
+          kan däremot göra att vi känner oss trötta, spända eller mindre
+          motiverade.
+        </p>
+        <p className="font-medium text-lg w-5xl pt-6 font-montserrat">
+          På vår plattform vill vi uppmuntra till återhämtning som fungerar i
+          vardagen. Genom att bli medveten om dina sömnvanor och små justeringar
+          i kvällsrutinerna, kan du skapa bättre balans.
+        </p>
+        <p className="font-medium text-lg w-5xl pt-6 font-montserrat">
+          Sömn är en vital del av återhämtning, att sova gott är avgörande för
+          både fysisk och mental hälsa. Nedan följer några praktiska råd som kan
+          förbättra din sömnkvalitet.
+        </p>
+
+        <p className="font-medium text-md w-5xl pt-6 font-montserrat">
+          1. Skapa en regelbunden sömnrutin <br /> Försök att gå och lägga dig
+          och vakna vid samma tid varje dag, även på helger. Det hjälper din
+          kropp att ställa in sin inre klocka och förbättrar sömnkvaliteten.
+        </p>
+        <p className="font-medium text-md w-5xl pt-6 font-montserrat">
+          2. Varva ner innan läggdags <br /> Undvik stimulerande aktiviteter som
+          att titta på skärmar eller arbeta sent på kvällen. Istället kan du
+          prova avslappningsövningar, läsa en bok eller ta ett varmt bad för att
+          signalera till kroppen att det är dags att sova.
+        </p>
+        <p className="font-medium text-md w-5xl pt-6 font-montserrat">
+          3. Fysisk aktivitet under dagen <br />
+          Regelbunden motion kan förbättra sömnen, men undvik intensiv träning
+          sent på kvällen då det kan göra det svårare att somna.
+        </p>
+        <p className="font-medium text-md w-5xl pt-6 font-montserrat">
+          4. Begränsa koffein och alkohol <br /> Undvik att konsumera
+          koffeinhaltiga drycker som kaffe, te och energidrycker samt alkohol
+          flera timmar innan läggdags, eftersom de kan störa sömnen.
+        </p>
+        <p className="font-medium text-md w-5xl pt-6 font-montserrat">
+          5. Exponera dig för dagsljus <br /> Att få naturligt ljus under dagen,
+          särskilt på morgonen, hjälper till att reglera din dygnsrytm och
+          förbättrar sömnkvaliteten.
+        </p>
+        <p className="font-medium text-md w-5xl pt-6 font-montserrat">
+          6. Hantera stress och oro <br /> Om du har mycket tankar som snurrar,
+          skriv ner dem i en dagbok innan du går och lägger dig. Det kan hjälpa
+          dig att släppa oron och sova bättre.
+        </p>
+        <p className="font-medium textmd w-5xl pt-6 font-montserrat">
+          7. Undvik tupplurar på dagen <br /> Om du har svårt att somna på
+          kvällen, försök att undvika att sova under dagen. Om du behöver vila,
+          håll tuppluren kort (20-30 minuter) och tidigt på eftermiddagen.
+        </p>
+      </div>
+    </div>
+  );
+}
